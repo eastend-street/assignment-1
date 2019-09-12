1ed70b30dff7b656caa1e77ef6c8c06fe41a33b2
eastend-street <ss_1322irf@icloud.com>
2019-09-12 16:46:01 -0700

    made pushing space button function

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ window.odometerOptions = {
 let slotID1 = 0;
 let slotID2 = 0;
 let slotID3 = 0;
+let spaceCount = 0;
 
 function loop_slot1() {
   $(".slot1").html(0);
@@ -24,29 +25,71 @@ function loop_slot3() {
   slotID3 = setTimeout(loop_slot3, 1950);
 }
 
-$("#spin").on("click", function() {
-  loop_slot1();
-  loop_slot2();
-  loop_slot3();
-});
-
-$("#stop1").on("click", function() {
+function stopSlot1() {
   clearTimeout(slotID1);
-  var random = Math.floor(Math.random() * 10);
+  const random = Math.floor(Math.random() * 10);
   $(".slot1").text(random);
   $(".slot1").html(random);
-});
+}
 
-$("#stop2").on("click", function() {
+function stopSlot2() {
   clearTimeout(slotID2);
-  var random = Math.floor(Math.random() * 10);
+  const random = Math.floor(Math.random() * 10);
   $(".slot2").text(random);
   $(".slot2").html(random);
-});
+}
 
-$("#stop3").on("click", function() {
+function stopSlot3() {
   clearTimeout(slotID3);
-  var random = Math.floor(Math.random() * 10);
+  const random = Math.floor(Math.random() * 10);
   $(".slot3").text(random);
   $(".slot3").html(random);
+  showResult();
+}
+
+function showResult(){
+  console.log("done!");
+}
+
+document.addEventListener("keydown", event => {
+  if (event.keyCode === 32) {
+    if (spaceCount === 0) {
+      loop_slot1();
+      loop_slot2();
+      loop_slot3();
+      spaceCount += 1;
+    } else if (spaceCount === 1) {
+      stopSlot1();
+      spaceCount += 1;
+    }
+    else if (spaceCount === 2) {
+      stopSlot2();
+      spaceCount += 1;
+    }
+    else if (spaceCount === 3) {
+      stopSlot3();
+      spaceCount = 0;
+    }
+  }
 });
+
+// $("#stop1").on("click", function() {
+//   clearTimeout(slotID1);
+//   var random = Math.floor(Math.random() * 10);
+//   $(".slot1").text(random);
+//   $(".slot1").html(random);
+// });
+
+// $("#stop2").on("click", function() {
+//   clearTimeout(slotID2);
+//   var random = Math.floor(Math.random() * 10);
+//   $(".slot2").text(random);
+//   $(".slot2").html(random);
+// });
+
+// $("#stop3").on("click", function() {
+//   clearTimeout(slotID3);
+//   var random = Math.floor(Math.random() * 10);
+//   $(".slot3").text(random);
+//   $(".slot3").html(random);
+// });
